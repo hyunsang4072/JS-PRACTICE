58df1f20f4e5bd44a852c5e2cc9ed1803cb98b40
hyunsang4072 <hyunsang4072@gmail.com>
2023-12-25 14:19:49 -0800

    random test

---
+++ randomText/script.js
@@ -0,0 +1,17 @@
+function tempting1() {
+    const elem = document.querySelector('.hidden1');
+    if (elem.style.display === 'none') {
+        elem.style.display = 'block';
+    } else {
+        elem.style.display = 'none';
+    }
+}
+
+function tempting2() {
+    const elem = document.querySelector('.hidden2');
+    if (elem.style.display === 'none') {
+        elem.style.display = 'block';
+    } else {
+        elem.style.display = 'none';
+    }
+}
